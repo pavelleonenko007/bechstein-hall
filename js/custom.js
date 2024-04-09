6f670778e2cf58f09236d4ba5a344928ec0fa967
pavelleonenko007 <pavel.leonenko374@gmail.com>
2024-04-10 00:18:33 +0500

    fix: bold text safari

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,10 @@
+function isSafariBrowser() {
+  return (
+    navigator.userAgent.indexOf('Safari') > -1 && 
+    navigator.userAgent.indexOf('Chrome') <= -1
+  );
+}
+
 document.querySelectorAll('.grey-z').forEach((trigger) => {
   new IntersectionObserver(
     (entries, observer) => {
