39217504a176aca92ab9dd80b4ece35974c69787
pavelleonenko007 <pavel.leonenko374@gmail.com>
2024-09-23 17:53:00 +0300

    fix: address of the building

--- footer.php
+++ footer.php
@@ -116,7 +116,7 @@ defined( 'ABSPATH' ) || exit; ?>
 							?>
 							<a href="<?php echo esc_url( bech_format_email_link( $email ) ); ?>"><?php echo esc_html( $email ); ?></a>
 						<?php endif; ?>
-						London, W1U 2RJ, 18 – 22 Wigmore Street
+						London, W1U 2RJ, 22 Wigmore Street
 					</div>
 				</div>
 				<?php
