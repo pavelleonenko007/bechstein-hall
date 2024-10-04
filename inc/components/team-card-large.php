2e3b2b1fe6686cb49583314ea305382c921e9841
pavelleonenko007 <pavel.leonenko374@gmail.com>
2024-10-04 16:40:15 +0500

    feat: add spotify link to teammate and remove linkedin link

--- inc/components/team-card-large.php
+++ inc/components/team-card-large.php
@@ -1,9 +1,9 @@
 <?php
 global $post;
 
-$linkedIn = get_field('linkedin');
 $instagram = get_field('instagram');
 $facebook = get_field('facebook');
+$spotify = get_field('spotify_url');
 ?>
 
 <div class="ui-big-man_left-col med">
@@ -20,14 +20,14 @@ $facebook = get_field('facebook');
 			<?php echo get_field('job_title') ?>
 		</div>
 		<div class="div-block-12">
-			<?php if (!empty($linkedIn)) : ?>
-				<a rel="nofollow" href="<?php echo $linkedIn; ?>" target="_blank" class="p-17-25 italic link-color">LinkedIn</a>
-			<?php endif; ?>
 			<?php if (!empty($instagram)) : ?>
-				<a rel="nofollow" href="<?php echo $instagram; ?>" target="_blank" class="p-17-25 italic link-color">Instagram</a>
+				<a rel="nofollow" href="<?php echo esc_url($instagram); ?>" target="_blank" class="p-17-25 italic link-color">Instagram</a>
 			<?php endif; ?>
 			<?php if (!empty($facebook)) : ?>
-				<a rel="nofollow" href="<?php echo $facebook; ?>" target="_blank" class="p-17-25 italic link-color">Facebook</a>
+				<a rel="nofollow" href="<?php echo esc_url($facebook); ?>" target="_blank" class="p-17-25 italic link-color">Facebook</a>
+			<?php endif; ?>
+			<?php if (!empty($spotify)) : ?>
+				<a rel="nofollow" href="<?php echo esc_url($spotify); ?>" target="_blank" class="p-17-25 italic link-color">Spotify</a>
 			<?php endif; ?>
 		</div>
 		<p class="p-20-30 people-p">
